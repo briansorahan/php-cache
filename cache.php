a9e8c4392b97632c30ed78e05c15d7f7cac28aec
Brian Sorahan <bsorahan@gmail.com>
2014-06-10 19:14:38 -0500

    first pass

--- cache.php
+++ cache.php
@@ -1,117 +1,301 @@
 <?php
 
 class Cache {
-  // entry is too large
-  public static $err_too_large = -10;
-  // attempted to add a key that already exists
-  public static $err_already_exists = -20;
+  private $key_size;
+  private $value_size;
+  private $max_mem;
+  // capacity in number of nodes
+  private $capacity;
+  // head and tail of doubly-linked list used to
+  // track least recently used values
+  private $head;
+  private $tail;
+  // storage
+  private $hash;
 
-  private static $max_key_size = 64;
-  private static $max_value_size = 256;
-  private static $max_entry_size;
-  private static $max_mem;
-
-  private $mem_used;
-  private $entries;
-
-  function __construct() {
-    $mem_used = 0;
-    $entries = array();
-    self::$max_entry_size = self::$max_key_size + self::$max_value_size;
-    self::$max_mem = 16 * 1024 * 1024;
+  // params
+  // 1. memory limit in Mb
+  // 2. key size in number of characters
+  // 3. value size in number of characters
+  function __construct($mm, $ks, $vs) {
+    $this->key_size = $ks;
+    $this->value_size = $vs;
+    $this->max_mem = $mm * 1024 * 1024;
+    $this->capacity = floor($this->max_mem / ($ks + $vs));
+    
+    $this->head = new CacheNode(NULL, NULL, NULL);
+    $this->tail = new CacheNode(NULL, NULL, NULL);
+    $this->hash = array();
   }
 
-  public function set($key, $value, $expire) {
-    $entry = new CacheEntry($value, $expire);
-
-    if ($entry->size() > MAX_ENTRY_SIZE) {
-      return self::$err_too_large;
-    } else if ($mem_used + $entry->size() > $max_mem) {
-      // try to free up memory for this entry
-      $this->evict($entry->size());
+  // get the value at key
+  // returns NULL if there is no value
+  public function get($key) {
+    if (empty($this->hash[$key])) {
+      return NULL;
     } else {
-      $entries[$key] = $entry;
-      $this->mem_used += $entry->size();
+      $node = $this->hash[$key];
+      $expires = $node->expires();
+      $now = time();
+
+      if (is_integer($expires) && $expires <= $now) {
+        // remove the expired value
+        unset($this->hash[$key]);
+        return NULL;
+      } else {
+        $this->setHead($node);
+        return $this->hash[$key]->getValue();
+      }
+    }
+  }
+
+  // set key to value
+  // expires in seconds from now
+  public function set($key, $value, $expires) {
+    if (count($this->hash) == $this->capacity) {
+      // get rid of the tail node
+      $this->removeNode($this->tail->getPrev());
     }
 
+    $node = new CacheNode(NULL, NULL, $expires);
+
+    $iv = intval($value);
+    $fv = floatval($value);
+
+    if ($iv == 0) {
+      // handle intval's shortcomings
+      if ($value == "0") {
+        $value = 0;
+      }
+    } else {
+      // don't convert to int if it's a float
+      if ($fv == $iv) {
+        $value = $iv;
+      }
+    }
+
+    $node->setValue($value);
+    $this->setHead($node);
+    $this->hash[$key] = $node;
     return $this;
   }
 
-  public function add($key, $value, $expire) {
-    $pre = $entries[$key];
-    if ($pre == NULL) {
-      return $this->set($key, $value, $expire);
+  // add key, value pair if it doesn't already exist
+  // if it does exist, return false
+  // return true otherwise
+  public function add($key, $value, $expires) {
+    if (! empty($this->hash[$key])) {
+      return false;
+    }
+    $this->set($key, $value, $expires);
+    return true;
+  }
+
+  // return true/false if the value was removed/absent
+  public function delete($key) {
+    if (empty($this->hash[$key])) {
+      return false;
+    }
+    // remove it from the lru list
+    $this->removeNode($this->hash[$key]);
+    // remove it from storage
+    unset($this->hash[$key]);
+    return true;
+  }
+
+  // attempt to increment an integer value
+  // return true if the value at key is an integer
+  // return false otherwise
+  public function increment($key, $amount = 1) {
+    if (empty($this->hash[$key])) {
+      return false;
+    }
+
+    $node = $this->hash[$key];
+    $val = $node->getValue();
+
+    if (!is_integer($val)) {
+      return false;
+    }
+
+    $node->setValue($val + $amount);
+    $this->setHead($node);
+    return true;
+  }
+
+  // attempt to decrement an integer value
+  // return true if the value at key is an integer
+  // return false otherwise
+  public function decrement($key, $amount = 1) {
+    if (empty($this->hash[$key])) {
+      return false;
+    }
+
+    $node = $this->hash[$key];
+    $val = $node->getValue();
+
+    if (!is_integer($val)) {
+      return false;
+    }
+
+    $node->setValue($val - $amount);
+    $this->setHead($node);
+    return true;
+  }
+
+  private function setHead($node) {
+    // remove it
+    $this->removeNode($node);
+    // add it to the head
+    $headNext = $this->head->getNext();
+    $this->head = $node;
+    $node->setNext($headNext);
+    $node->setPrev(NULL);
+  }
+
+  // remove a node from the lru list
+  private function removeNode($node) {
+    $prev = $node->getPrev();
+    $next = $node->getNext();
+    if (is_object($prev)) {
+      $prev->setNext($next);
+    }
+    if (is_object($next)) {
+      $next->setPrev($prev);
+    }
+  }
+}
+
+// doubly linked-list node
+class CacheNode {
+  private $prev;
+  private $next;
+  private $expires;
+  private $value;
+
+  function __construct($p, $n, $exp) {
+    $this->prev = $p;
+    $this->next = $n;
+    
+    if (is_null($exp) || empty($exp)) {
+      $this->expires = NULL;
     } else {
-      return
+      $this->expires = time() + $exp;
     }
   }
 
-  public function get($key) {
-    return $entries[$key]->value();
-  }
+  public function getPrev() { return $this->prev; }
+  public function setPrev($p) { $this->prev = $p; }
 
-  // free up storage for an object
-  // favors (from highest to lowest)
-  // 1. Already expired
-  // 2. Least Recently Used
-  // 3. Soonest expiration
-  private function evict($size) {
-    // key to be removed
-    $remove = NULL;
-    // key of least recently used item
-    $lru_key = NULL;
-    $lru_time = 0;
-    // key for entry with soonest expiration
-    $soonest_key = NULL;
-    $soonest_time = PHP_INT_MAX;
-    $now = time();
+  public function getNext() { return $this->next; }
+  public function setNext($n) { $this->next = $n; }
 
-    foreach($entries as $key => $en) {
-      if ($now <= $en->expire()) {
-        $remove = $key;
-        break;
-      }
-      $last_used = $en->last_used();
-      if ($last_used > $lru_time) {
-        $lru_time = $last_used;
-      }
-      $expire = $en->expire();
-      if ($expire
-    }
-  }
+  public function getValue() { return $this->value; }
+  public function setValue($v) { $this->value = $v; }
 
-  private function add($entry) {
-  }
+  public function expires() { return $this->expires; }
 }
 
-class CacheEntry {
-  private $m_value;
-  private $m_size;
-  private $m_expire;
-  private $m_last_used;
-
-  function __construct($value, $expire) {
-    $now = time();
-    $m_value = $value;
-    $m_expire = $now + $expire;
-    $m_size = strlen($m_value);
-  }
-
-  public function size() { return $m_size; }
-  public function expire() { return $m_expire; }
-  public function value() { return $m_value; }
-  public function last_used() { return $m_last_used; }
-
-  public function mark_used() {
-    $m_last_used = time();
-  }
-}
-
-$cache = new Cache();
+$cache = new Cache(16, 64, 255);
 
 echo "> ";
 
 while ($c = fread(STDIN, 512)) {
+  $stored = "STORED\n";
+  $error = "ERROR\n";
+
+  $pieces = preg_split("/\s+/", $c);
+  $num_pieces = count($pieces);
+
+  if ($num_pieces > 0) {
+    $cmd = $pieces[0];
+    $key = $pieces[1];
+
+    if (empty($cmd)) {
+      echo "> ";
+      continue;
+    }
+
+    if (empty($key)) {
+      echo "ERROR\n> ";
+      continue;
+    }
+
+    switch(strtolower($cmd)) {
+      case "get":
+        $val = $cache->get($key);
+        if (!is_null($val)) {
+          echo $val . "\n";
+        }
+        break;
+      case "set":
+        if ($num_pieces < 3 || $pieces[2] == "") {
+          // set requires key and value
+          echo $error;
+          break;
+        }
+
+        $expires = NULL;
+        if ($num_pieces >= 4) {
+          $expires = $pieces[3];
+        }
+
+        $cache->set($key, $pieces[2], $expires);
+        echo $stored;
+        break;
+      case "add":
+        if ($num_pieces < 3 || $pieces[2] == "") {
+          // add requires key and value
+          echo $error;
+          break;
+        }
+
+        $expires = NULL;
+        if ($num_pieces >= 4) {
+          $expires = $pieces[3];
+        }
+
+        if ($cache->add($key, $pieces[2], $expires)) {
+          echo $stored;
+        } else {
+          echo $error;
+        }
+        break;
+      case "increment":
+        $amount = 1;
+        if ($num_pieces >= 3 && !empty($pieces[2])) {
+          $amount = intval($pieces[2]);
+        }
+
+        if ($cache->increment($key, $amount)) {
+          echo $cache->get($key) . "\n";
+        } else {
+          echo $error;
+        }
+        break;
+      case "decrement":
+        $amount = 1;
+        if ($num_pieces >= 3 && !empty($pieces[2])) {
+          $amount = intval($pieces[2]);
+        }
+
+        if ($cache->decrement($key, $amount)) {
+          echo $cache->get($key) . "\n";
+        } else {
+          echo $error;
+        }
+        break;
+      case "delete":
+        if ($cache->delete($key)) {
+          echo "DELETED\n";
+        } else {
+          echo $error;
+        }
+        break;
+      default:
+        echo "Unrecognized command " . $cmd . "\n";
+    }
+  }
   echo "> ";
 }
 
